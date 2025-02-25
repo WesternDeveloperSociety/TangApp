d34058b5dcc6fc3912e8d5380422385ef379e340
Thomson-Lam <thomson.lam327@gmail.com>
2025-02-24 19:53:09 -0500

    Edited map component for Web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,43 @@
-import { Text, View } from 'react-native';
-// import MapView from 'react-native-maps';
+import React from 'react';
+import { StatusBar, View, Dimensions, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function HomeScreen() {
-  return (
-    <>
-    <View className="bg-cyan-200 m-auto w-screen h-screen flex-auto justify-center">
-    <Text numberOfLines={1} className="color-white text-center font-bold">The home page, Edit me</Text>
-    </View>
-    </>
-  );
+
+export default class HomeScreen extends React.Component {
+
+  // makes screen properly fullscreen for curved phones
+  componentDidMount() {
+    StatusBar.setTranslucent(true);
+    StatusBar.setBackgroundColor("transparent");
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 43.004341,
+            longitude: -81.276023,
+            // effects zoom
+            latitudeDelta: 0.0015,
+            longitudeDelta: 0.0015,
+          }}
+        />
+      </View>
+    );
+  }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
